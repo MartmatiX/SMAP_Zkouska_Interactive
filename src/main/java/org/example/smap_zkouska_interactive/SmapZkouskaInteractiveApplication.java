e08414e77b1de9481ee2844a7f0abff168c491fb
MartmatiX <martin.malir@gmail.com>
2025-05-01 19:00:19 +0200

    Initial commit

---
+++ src/main/java/org/example/smap_zkouska_interactive/SmapZkouskaInteractiveApplication.java
@@ -0,0 +1,13 @@
+package org.example.smap_zkouska_interactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapZkouskaInteractiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmapZkouskaInteractiveApplication.class, args);
+    }
+
+}
